10f22b5b4978136a6e9685fda9b9ce9451a5f69b
Jack Kearney <jkearney@oxy.edu>
2019-09-30 21:28:53 +1300

    Added shell scripts for link and play servers

---
+++ linkServer.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install -y apache2 php libapache2-mod-php php-mysql
+cp /vagrant/linkServer.conf /etc/apache2/sites-available/
+a2ensite test-website
+a2dissite 000-default
+service apache2 reload
